4ddc5a34d849b5b376879699c2299d07f1ab03ed
happiness <happinesskawinzi@gmail.com>
2019-10-06 18:45:06 +0300

    creating the first model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    Image_name = models.CharField(max_length = 300)
+    Image_description = models.CharField(max_length = 300)
+    Image_location = models.CharField(max_length = 300)
+    Image_category =  models.CharField(max_length = 300)
+    
