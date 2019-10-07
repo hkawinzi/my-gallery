9cd0a630e965d6790e55089e1d6415700c23c838
happiness <happinesskawinzi@gmail.com>
2019-10-07 08:19:55 +0300

    adding models to the admin page

--- photos/admin.py
+++ photos/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Image,Comment,tags
+from .models import Editor,Article,tags
 
 # Register your models here.
 admin.site.register(Image)
-admin.site.register(Comment)
+admin.site.register(Picture)
 admin.site.register(tags)
