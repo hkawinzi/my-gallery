7d26d5bb6c3f21bfe895392310d611cb18bf343c
happiness <happinesskawinzi@gmail.com>
2019-10-07 10:38:30 +0300

    creating a 404 error template

--- photos/views.py
+++ photos/views.py
@@ -5,3 +5,24 @@ from django.http  import HttpResponse
 def welcome(request):
     return render(request, 'welcome.html')
 
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-photos/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-photos/search.html',{"message":message})
+
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-photos/image.html", {"image":image})
+
+
